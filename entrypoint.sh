3bd113e27d142fadd792257cf6c58fa7eec6a33c
twiddler <13364827+twiddler@users.noreply.github.com>
2022-02-07 23:47:20 -0500

    $(...)-ify jq call

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ OWN=$(skopeo inspect docker://"$1" | jq .Layers)
 PARENT=$(skopeo inspect docker://"$2" | jq .Layers)
 
 # Check if all layers of parent are present in my image. If not, mine is out-of-date.
-OUTOFDATE=jq -cn "$OWN - ($OWN - $PARENT) | .==[]"
+OUTOFDATE=$(jq -cn "$OWN - ($OWN - $PARENT) | .==[]")
 
 # Return the result.
 echo "::set-output name=time::$OUTOFDATE"
