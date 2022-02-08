f3b5841daee247288cc4843181c9d31b9acdb69a
twiddler <13364827+twiddler@users.noreply.github.com>
2022-02-08 14:23:39 -0500

    Invert exit code; add $FILENAME

--- tests/assert-out-of-date.sh
+++ tests/assert-out-of-date.sh
@@ -1,10 +1,13 @@
+FILENAME=$(basename $BASH_SOURCE)
+
 # Only out-of-date combinations of `my-image` and `parent-image` from the test workflow should end up here. If any other combination ends up here, we need to throw an error.
 
-echo "This script only runs if the check thinks my image is out-of-date. Let's see if our expectations agree."
+echo "[$FILENAME] This script only runs if the check thinks my image is out-of-date. Let's see if our expectations agree."
 
 OWN=$1
 PARENT=$2
 
-[ "$OWN" = "nginx/nginx-ingress:1.12.0" ] && [ "$PARENT" = "nginx:1.21.0" ] && exit 0
+# There are fewer up-to-date combinations to check. Throw an error if we encounter one of those.
+[ "$OWN" = "nginx/nginx-ingress:1.12.0" ] && [ "$PARENT" = "nginx:1.21.0" ] && exit 1
 
-exit 1
+echo "[$FILENAME] All tests passed."
--- tests/assert-up-to-date.sh
+++ tests/assert-up-to-date.sh
@@ -1,6 +1,11 @@
+FILENAME=$(basename $BASH_SOURCE)
+
 # Only up-to-date combinations of `my-image` and `parent-image` from the test workflow should end up here. If any other combination ends up here, we need to throw an error.
 
-echo "This script only runs if the check thinks my image is up-to-date. Let's see if our expectations agree."
+echo "[$FILENAME] This script only runs if the check thinks my image is up-to-date. Let's see if our expectations agree."
 
 # This should be the exact inverse of the other script.
+echo "[$FILENAME] We will call the other script and invert its exit code."
 ./tests/assert-out-of-date.sh $1 $2 && exit 1
+
+echo "[$FILENAME] All tests passed."
